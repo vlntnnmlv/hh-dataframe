458dd42b9bcbaca56a783a6aeeb207de697c6ec3
postironiya <51166600+postironiya@users.noreply.github.com>
2021-12-04 21:17:29 +0300

    Added async requests

--- load_vacancies.py
+++ load_vacancies.py
@@ -1,6 +1,10 @@
+from asyncio.tasks import FIRST_EXCEPTION
+from asyncio.windows_events import NULL
 import requests
 import pickle
 import logging
+
+from concurrent.futures import ThreadPoolExecutor, wait, FIRST_EXCEPTION
 from lxml.html import fromstring
 from itertools import cycle
 from time import sleep
@@ -8,9 +12,16 @@ from time import sleep
 # hh.ru API domain
 main_domain = "https://api.hh.ru/"
 
-# logger object
-logger = logging.getLogger("hhparser")
-logger.setLevel(logging.INFO)
+def getVacancy(id, vacancy_list, executor):
+    res = requests.get(f"{main_domain}vacancies/{id}")
+    print(f"Got response on id {id}")
+    if res.status_code == 200:
+        print("Got nice reponse, processing...")
+        vacancy_list.append(res.json())
+    elif res.status_code == 403:
+        print("Got 403 status code")
+        # raise Exception
+        executor.shutdown()
 
 def getVacancies(ids, prev_vcs):
     vcs = prev_vcs
@@ -19,25 +30,34 @@ def getVacancies(ids, prev_vcs):
     else:
         i = 0
     print(f"Starting index is {i} in id {ids[i]}")
-    j = 0
-    print("Started request loop")
-    while i < len(ids):
-        res = requests.get(f"{main_domain}vacancies/{ids[i]}")
-        print(f"Got response on id {ids[i]}")
-        if res.status_code == 200:
-            j = 0
-            print("Got nice reponse, processing...")
-            vcs.append(res.json())
-        elif res.status_code == 403:
-            print("Got 403 status code")
-            return vcs
-        
-        sleep(0.1)
-        i+=1
 
+    print("Started future creation loop")
+    futures = []
+    with ThreadPoolExecutor() as executor:
+        while i < len(ids):
+            try:
+                futures.append(
+                        executor.submit(getVacancy, ids[i], vcs, executor)
+                    )
+            except Exception as e:
+                print(f"Got exception:\n{e}\nBreaking future creation loop...")
+                break
+
+            # print(f"Got response on id {ids[i]}")
+            # if res.status_code == 200:
+            #     print("Got nice reponse, processing...")
+            #     vcs.append(res.json())
+            # elif res.status_code == 403:
+            #     print("Got 403 status code")
+            #     return vcs
+            
+            sleep(0.1)
+            i+=1
+    
+    print("Waiting for created futures to complete...")
+    wait(futures, return_when=FIRST_EXCEPTION)
+    print("Done!")
     return vcs
-
-
 if __name__ == '__main__':
     print("Loading ids...")
     with open("02_12_2021_vacancies_id", "rb") as inp:
@@ -52,10 +72,7 @@ if __name__ == '__main__':
     except Exception:
         vcs = []
 
-    if vcs:
-        print("Found local vacancies")
-    else:
-        print("Local vacancies are empty")
+    print("Found local vacancies") if vcs else print("Local vacancies are empty")
 
     vcs = getVacancies(ids, vcs)
 
